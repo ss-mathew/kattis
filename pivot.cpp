cd7380d360926c3f3f9781f18d32c36e8f7dcff1
ss-mathew <70663265+ss-mathew@users.noreply.github.com>
2023-04-24 19:49:56 -0400

    pivot.cpp

---
+++ pivot.cpp
@@ -0,0 +1,43 @@
+/*
+  Author: Sheryl Mathew
+  It is not okay to share my code anonymously for educational purposes
+*/
+
+#include <iostream>
+#include<algorithm>
+#include<vector>
+
+int main() {
+  int n;
+  std::cin >> n;
+  std::vector<int> A(n);
+  std::vector<int> Aprime(n);
+  std::vector<int> rightmostMin(n);
+  int total = 0; //# of possible pivots
+  for (int x = 0; x < n; x++) { //parse inp
+        int add;
+        std::cin >> add;
+        A[x] = add;
+        Aprime[x] = add;
+    }
+    std::sort(Aprime.begin(), Aprime.end()); //one Arr sorted
+    
+    int min = A[n-1];
+    for (int x = n-1; x >=0; x--) { //vector filled with the min starting from the end to that position
+        min = std::min(min, A[x]);
+        rightmostMin[x] = min;
+    }
+     for (int x = 0; x < n; x++) {
+         if (A[x] == Aprime[x]) { //check pivot is in the same position after sorting
+             if (x == n-1) { //last index pivot case
+                 total++;
+             } else if (A[x] == Aprime[x] && (rightmostMin[x+1] > A[x])) { //check that min of values post pivot > pivot
+             total++;
+            }
+             
+         }
+    }
+        std::cout << total;
+        return 0;
+}
+
